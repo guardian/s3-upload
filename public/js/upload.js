10ae78fc71fc66d5c50886bbd9f6c9f0c20eea2f
Akash A <akash1810@gmail.com>
2016-07-10 12:09:33 +0100

    added dropzone.js

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,25 +1,16 @@
-document.addEventListener("DOMContentLoaded", init, false);
-
 function init() {
-    document.querySelector('#fileChooser').addEventListener('change', handleFileSelect, false);
+    Dropzone.autoDiscover = false;
+
+    var el = document.querySelector('.file-chooser');
+    el.classList.add('dropzone');
+
+    var dropzoneEl = new Dropzone(el, {
+        init: function () {
+            this.on("success", function () {
+                console.log('yay');
+            });
+        }
+    });
 }
 
-function handleFileSelect(event) {
-    if(!event.target.files) return;
-    const files = event.target.files;
-    const selected = document.querySelector("#selectedFiles ul");
-    selected.innerHTML = "";
-
-    for(var i=0; i<files.length; i++) {
-      var file = files[i];
-      var ext = file.name.match(/\.(.+)$/)[1];
-      if(ext == "png"){
-        document.querySelector("#png-error").classList.remove("upload-error--hidden");
-      }
-
-      var li = document.createElement('li')
-      li.appendChild(document.createTextNode(file.name));
-      selected.appendChild(li);
-
-    }
-}
+document.addEventListener("DOMContentLoaded", init, false);
