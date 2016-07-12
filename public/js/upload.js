cb8a451d2f9e6f57043d15cf43f7ac4775ee3a5b
Akash A <akash1810@gmail.com>
2016-07-12 21:27:58 +0100

    wip

--- public/js/upload.js
+++ public/js/upload.js
@@ -4,7 +4,10 @@ function init() {
     var el = document.querySelector('.file-chooser');
     el.classList.add('dropzone');
 
+    var previewTmpl = document.querySelector('.dz-template').innerHTML;
+
     var dropzoneEl = new Dropzone(el, {
+        previewTemplate: previewTmpl,
         init: function () {
             this.on("success", function () {
                 console.log('yay');
