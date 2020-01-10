e3ea0d5dbeb758b2fe5a4d49de186607aa43e89f
Anna Leach <anna.leach@guardian.co.uk>
2020-01-10 15:47:20 +0000

    add cors line back in

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -6,7 +6,7 @@ import play.api.BuiltInComponentsFromContext
 import play.api.mvc.EssentialFilter
 import play.api.routing.Router
 import play.filters.HttpFiltersComponents
-import play.filters.cors.{CORSComponents}
+import play.filters.cors.{CORSComponents, CORSConfig}
 import router.Routes
 
 
@@ -21,6 +21,10 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
 
   val api = new Application(s3Actions, publicSettings, controllerComponents)
   val management = new Management(controllerComponents)
+
+  override lazy val corsConfig: CORSConfig = CORSConfig.fromConfiguration(configuration)
+
+  println("cors config is --->", corsConfig)
 
   val disabledFilters: Set[EssentialFilter] = Set(allowedHostsFilter)
   final override def httpFilters: Seq[EssentialFilter] = corsFilter +: super.httpFilters.filterNot(disabledFilters.contains)
