7a248a0ba85f0164f11438516e0f78ff1a065c94
Roberto Tyley <52038+rtyley@users.noreply.github.com>
2024-08-08 15:02:29 +0100

    Try out updated Panda settings code

--- app/lib/PanAuth.scala
+++ app/lib/PanAuth.scala
@@ -18,43 +18,34 @@ trait PandaController extends BaseControllerHelpers with Logging {
     Future.successful(Unauthorized(views.html.login(S3UploadAppConfig.loginUri)(request)))
   }
 
-  def authStatus(cookie: Cookie, publicKey: PublicKey): AuthenticationStatus = {
-    PanDomain.authStatus(
-      cookie.value,
-      publicKey,
-      PanDomain.guardianValidation,
-      apiGracePeriod = 0,
-      system = "s3-upload",
-      cacheValidation = false,
-      forceExpiry = false
-    )
-  }
+  def authStatus(cookie: Cookie, publicKey: PublicKey): AuthenticationStatus = PanDomain.authStatus(
+    cookie.value,
+    publicKey,
+    PanDomain.guardianValidation,
+    apiGracePeriod = 0,
+    system = "s3-upload",
+    cacheValidation = false,
+    forceExpiry = false
+  )
 
   object AuthAction extends ActionBuilder[UserRequest, AnyContent] {
     override def parser: BodyParser[AnyContent] = PandaController.this.controllerComponents.parsers.default
     override protected def executionContext: ExecutionContext = PandaController.this.controllerComponents.executionContext
 
     override def invokeBlock[A](request: Request[A], block: UserRequest[A] => Future[Result]): Future[Result] = {
-      publicSettings.publicKey match {
-        case Some(pk) =>
-          request.cookies.get("gutoolsAuth-assym") match {
-            case Some(cookie) =>
-              authStatus(cookie, pk) match {
-                case Authenticated(AuthenticatedUser(user, _, _, _, _)) =>
-                  block(new UserRequest(user, request))
+      request.cookies.get("gutoolsAuth-assym") match {
+        case Some(cookie) =>
+          authStatus(cookie, publicSettings.publicKey) match {
+            case Authenticated(AuthenticatedUser(user, _, _, _, _)) =>
+              block(new UserRequest(user, request))
 
-                case other =>
-                  logger.info(s"Login response $other")
-                  unauthorisedResponse(request)
-              }
-
-            case None =>
-              logger.warn("Panda cookie missing")
+            case other =>
+              logger.info(s"Login response $other")
               unauthorisedResponse(request)
           }
 
         case None =>
-          logger.error("Panda public key unavailable")
+          logger.warn("Panda cookie missing")
           unauthorisedResponse(request)
       }
     }
